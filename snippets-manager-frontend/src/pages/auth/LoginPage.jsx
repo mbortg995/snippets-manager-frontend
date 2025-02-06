694c77ead706eed2ca4194a4edd8815016884f46
mbortg995 <mbortg995@gmail.com>
2025-02-06 11:41:24 +0100

    feat: now showing login page, but some styling needs to be done yet.

--- snippets-manager-frontend/src/pages/auth/LoginPage.jsx
+++ snippets-manager-frontend/src/pages/auth/LoginPage.jsx
@@ -1,10 +1,41 @@
 import { Link } from "react-router-dom";
+import '@/assets/css/reset.css'
+import '@/assets/css/variables.css'
+import '@/assets/css/styles.css'
+import '@/assets/css/auth.css'
+import '@/assets/css/layout.css'
 
 const LoginPage = () => {
   return (
     <>
-      <p>Estoy en el Login!</p>
-      <Link to="/register">¿No tienes cuenta? Accede aquí al registro.</Link>
+      <div className="auth-layout">
+        <div className="auth-image">
+          <div className="auth-image-overlay"></div>
+          <h2 className="auth-tagline">Guarda tus snippets.Comparte tu código.</h2>
+        </div>
+
+        <div className="auth-content">
+          <div className="auth-header">
+            <img src="/images/logo.png" alt="Logo" className="auth-logo" />
+            <h1 className="auth-title">Iniciar sesión</h1>
+            <p className="auth-subtitle">
+              No tienes una cuenta? <a href="/register.html">Registrate</a>
+            </p>
+          </div>
+
+          <form className="auth-form">
+            <div className="auth-alert" id="error-message"></div>
+            <div className="auth-form-group">
+              <input type="email" id="email" name="email" placeholder="Correo electrónico" required />
+            </div>
+            <div className="auth-form-group">
+              <input type="password" id="password" name="password" placeholder="Contraseña" required />
+            </div>
+
+            <button type="submit" className="auth-button">Iniciar sesión</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }

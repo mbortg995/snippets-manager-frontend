3ac6232f31cd913eddb238d01bcec526f64de879
mbortg995 <mbortg995@gmail.com>
2025-02-08 11:09:48 +0100

    feat: create snippets

--- snippets-manager-frontend/src/App.jsx
+++ snippets-manager-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SnippetsListPage from '@/pages/snippets/SnippetsListPage.jsx'
 import LoginPage from '@/pages/auth/LoginPage.jsx'
 import RegisterPage from '@/pages/auth/RegisterPage.jsx'
+import CreateSnippet from '@/pages/snippets/CreateSnippet.jsx'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<SnippetsListPage />} ></Route>
         <Route path="/login" element={<LoginPage />} ></Route>
         <Route path="/register" element={<RegisterPage />} ></Route>
+        <Route path="/create" element={<CreateSnippet />} ></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ snippets-manager-frontend/src/pages/snippets/CreateSnippet.jsx
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const CreateSnippet = () => {
+
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    category: "",
+    content: "",
+    author: ""
+  });
+
+  const handleInputChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    });
+  }
+  const [error, setError] = useState("");
+
+  const handleSubmitForm = async (event) => {
+    event.preventDefault();
+    console.log(data);
+    try {
+      const response = await fetch('https://snippets-manager-ft.onrender.com/api/snippets', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error.error);
+        return;
+      }
+
+      navigate('/');
+    } catch {
+      setError("Servicio no disponible. Inténtelo de nuevo más adelante.");
+    }
+  }
+
+  return (
+    <div className="create-layout">
+      <header className="create-header">
+        <div className="header-content">
+          <img src="/images/logo.png" alt="Logo" />
+          <a href="/" className="back-link">← Volver a la Lista</a>
+        </div>
+      </header>
+
+      <main className="create-content">
+        <form
+          className="create-form"
+          onSubmit={handleSubmitForm}
+        >
+          <div className="form-group">
+            <label htmlFor="title">Título</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={data.title}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Descripción</label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={data.description}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="language">Lenguaje</label>
+            <select
+              id="language"
+              name="category"
+              value={data.category}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Selecciona un lenguaje</option>
+              <option value="nodejs">Node.js</option>
+              <option value="javascript">JavaScript</option>
+              <option value="html">HTML</option>
+              <option value="css">CSS</option>
+              <option value="express">Express</option>
+              <option value="npm">NPM</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="code">Código</label>
+            <textarea
+              id="code"
+              name="content"
+              value={data.content}
+              onChange={handleInputChange}
+              required
+            ></textarea>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="author">Autor</label>
+            <select
+              id="author"
+              name="author"
+              value={data.author}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Selecciona un autor</option>
+              <option value="Miguel">Miguel</option>
+              <option value="Cristian">Cristian</option>
+              <option value="Larry">Larry</option>
+              <option value="Pepe">Pepe</option>
+              <option value="Jona">Jona</option>
+            </select>
+          </div>
+          {error ? <div className="auth-alert" id="error-message">{error}</div> : null}
+          <div className="form-actions">
+            <button type="submit" className="submit-button">Crear Snippet</button>
+            <a href="/" className="cancel-button">Cancelar</a>
+          </div>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default CreateSnippet;
